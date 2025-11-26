693324d6cfbfc41bc03d44b0371153698c42d795
Adam Morris <amorris@mistermorris.com>
2025-11-26 16:42:55 +0800

    doc added

--- data2insights/sheets.py
+++ data2insights/sheets.py
@@ -1,27 +1,37 @@
 from google.colab import auth
 from google.auth import default
 import gspread
+from dataclasses import dataclass, field
 
 
+@dataclass
+class Doc:
+    spreadsheet: gspread.Spreadsheet
+
+
+@dataclass
 class GSheet:
     """
-    Interact with a spreadsheet
+    Initialize a spreadsheet for convenience
     """
 
-    def __init__(self, spreadsheet_id: str, open_: bool = True):
+    spreadsheet_id: str  # req
+    open_: bool = True  # open by default
+    document: Doc = field(init=False)  # let me init
+
+    def __post_init__(self, open_: bool = True):
         """
         Authenticate with google and authorize utiltites
         spreadsheet_id: The ID from the URL of the spreadsheet
 
         -> Will have .document
         """
-        self.spreadsheet_id = spreadsheet_id
         auth.authenticate_user()
         self.creds, _ = default()
         gc = gspread.authorize(self.creds)
         if open_:
             self.open_by_key()
 
-    def open_by_key(self):
-        self.document = gc.open_by_key(self.spreadsheet_id)
+    def open_by_key(self) -> Doc:
+        self.document = Doc(gc.open_by_key(self.spreadsheet_id))
         return self.document
